16fa9db063a074ee3c9053b28c997e3a55a74309
Ryabov Nikita <nikitos.aka.zed@gmail.com>
2019-04-20 15:19:33 +0300

    graphQL API SW added

--- README.md
+++ README.md
@@ -11,3 +11,26 @@ query {
   hello
 }
 ```
+
+`@playlyfe/gql` - для синтаксиса
+
+`npm install -D @babel/core @babel/preset-env babel-watch @playlyfe/gql`
+
+.babelrc
+
+```js
+{
+  "presets": ["@babel/preset-env"]
+}
+```
+
+package.json
+
+```json
+"scripts": {
+    "dev": "babel-watch index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  }
+```
+
+Будет смотреть и автоматические перезагружать при изменениях (возможно также для этого использовать nodemon)
--- index.js
+++ index.js
@@ -1,36 +1,20 @@
-const express = require("express");
-/**
- * gql - тэг шаблона, использующийся для написания схемы GraphQL
- */
-const { ApolloServer, gql } = require("apollo-server-express");
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
 
-/**
- * В схеме должны быть указаны типы для чтения данных,
- * а также может иметь атрибуты, в которые вложены другие атрибуты
- */
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-/**
- * Используются для возврата данных из схемы
- */
-const resolvers = {
-  Query: {
-    hello: () => "Hello world!"
-  }
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
+import typeDefs from "./api/schema";
+import resolvers from "./api/resolvers";
 
 const app = express();
-/**
- * для соединения express и ApolloServer
- */
-server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+const PORT = 4000;
+
+const SERVER = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+SERVER.applyMiddleware({ app });
+
+app.listen(PORT, () =>
+  console.log(`🚀 GraphQL playground is running at http://localhost:4000`)
 );
